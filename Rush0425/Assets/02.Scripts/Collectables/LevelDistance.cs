349153014afd246016f51f72724f77823cbe044f
jayeongcho <chojy0216@gmail.com>
2024-05-28 16:32:28 +0900

    5.28
    5.28

--- Rush0425/Assets/02.Scripts/Collectables/LevelDistance.cs
+++ Rush0425/Assets/02.Scripts/Collectables/LevelDistance.cs
@@ -1,19 +1,19 @@
+using DG.Tweening;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
-using DG.Tweening;
 
 public class LevelDistance : MonoBehaviour
 {
     //거리UI
-   // public GameObject disDisplay;
-   // public GameObject disEndDisplay;
+    // public GameObject disDisplay;
+    // public GameObject disEndDisplay;
     public int disRun;
     public bool addingDis = false;
     public float disDelay = 0.35f; //1m를 0.35초마다 돌파
-    public float divNum ;
+    public float divNum;
     // 별UI
-    public Image [] starImages; //별이미지
+    public Image[] starImages; //별이미지
     public Sprite yellowStarSprite; //노란별
     public Sprite greyStarSprite;//회색별
     public int starsCollected = 0;
@@ -31,7 +31,7 @@ public class LevelDistance : MonoBehaviour
 
     //슬라이더
     public Slider slider;
-    float barMaxValue =100f;
+    float barMaxValue = 100f;
     public float disRunUI;
 
     //흔들기
@@ -46,53 +46,44 @@ public class LevelDistance : MonoBehaviour
     }
     void Update()
     {
-        if (addingDis == false  && Toony_PlayerMove.canMove)
+        if (addingDis == false && Toony_PlayerMove.canMove)
         {
             addingDis = true;
             StartCoroutine(AddingDis());
-            
+
         }
-        
+
     }
 
     //m마다 거리추가
     IEnumerator AddingDis()
     {
         disRun += 1;
-       // disDisplay.GetComponent<Text>().text = "" + disRun; //게임 중 거리 텍스트 
+        // disDisplay.GetComponent<Text>().text = "" + disRun; //게임 중 거리 텍스트 
         //disEndDisplay.GetComponent<Text>().text = "" + disRun; //게임 끝나고 나오는 텍스트
 
         // 별 획득 체크
         if (disRun % divNum == 0)
         {
             starsCollected++;
-
-            
             PlayerPrefs.SetInt("Lv" + level, starsCollected);
-           
             UpdateStarUI();
-            
-
         }
-        if(slider.value != barMaxValue)
+        if (slider.value != barMaxValue)
         {
             disRunUI += 1;
-            slider.value = disRunUI * (barMaxValue/divNum);
+            slider.value = disRunUI * (barMaxValue / divNum);
         }
         else if (slider.value == barMaxValue)
         {
-            Debug.Log("");
             disRunUI = 1;
             slider.value = 1f;
         }
-
-
         yield return new WaitForSeconds(disDelay);
         addingDis = false;
-
     }
 
-   public void UpdateStarUI()
+    public void UpdateStarUI()
     {
 
         //별을 획득한 경우
@@ -109,21 +100,21 @@ public class LevelDistance : MonoBehaviour
             {
                 // 획득하지 않은 별은 회색으로 유지
                 starImages[i].sprite = greyStarSprite;
-                EndstarImages[i].sprite= greyStarSprite;
+                EndstarImages[i].sprite = greyStarSprite;
             }
 
             //획득시 흔들기
             Debug.Log("star[i]" + starImages[i]);
             starImages[i].transform.DOShakePosition(duration, strength, vibrato);
         }
-       
+
 
         // 모든 별을 획득한 경우 메시지 출력
         if (starsCollected == starsToCollect)
         {
             Debug.Log("모든 별을 획득했습니다!");
             //별을 모두 모으면 게임종료
-           playerMoveScript.ColletedAll();
+            playerMoveScript.ColletedAll();
 
         }
     }
